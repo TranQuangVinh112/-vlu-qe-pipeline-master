94997adcc8d35b3023b7fe8a6dcbee027e3affbe
TranQuangVinh112 <quangvinh05052006htn@gmail.com>
2026-09-21 13:35:06 +0700

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,25 @@
+function login(username, password) {
+    if (username === "admin" && password === "123") {
+        return true;
+    }
+
+    if (username === "") {
+        throw new Error("Username cannot be empty");
+    }
+
+    if (password === "") {
+        throw new Error("Password cannot be empty");
+    }
+
+    if (password.includes("!")) {
+        throw new Error("Password contains special character");
+    }
+
+    if (username === "locked") {
+        throw new Error("Account is locked");
+    }
+
+    return false;
+}
+
+module.exports = { login };
